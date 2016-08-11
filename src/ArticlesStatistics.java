74d8d1b8e088ee509e5cb63b40cc064494ac6cc2
luckynick <arm454h@gmail.com>
2016-08-11 13:42:27 +0300

    1st attempt to repair SocketTimeOutException handling (moved loop to thread)

--- src/ArticlesStatistics.java
+++ src/ArticlesStatistics.java
@@ -4,6 +4,8 @@ import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 import java.io.*;
+import java.net.MalformedURLException;
+import java.net.SocketTimeoutException;
 import java.net.URL;
 import java.nio.charset.Charset;
 import java.nio.file.Files;
@@ -20,56 +22,67 @@ public class ArticlesStatistics
     static final String statisticsPath = "statistics.txt";
     static final String logPath = "log.txt";
 
+    volatile boolean execute = true;
+    volatile int c = 0;
 
 
     public static void main(String args[]) throws Exception
     {
         ArticlesStatistics as = new ArticlesStatistics();
 
-        boolean execute = true;
-        int c = 0;
-        while(execute && c < 10)
+        try
         {
-            execute = false;
-            try
-            {
-                new Thread( () -> {
+            new Thread(() -> {
+                while(as.execute && as.c < 10)
+                {
+                    as.execute = false;
                     //as.backup(dataBasePath);
 
                     try
                     {
                         as.appendToDataBase(as.getNewTitles());
                     }
+                    catch(SocketTimeoutException e)
+                    {
+                        as.execute = true;
+                        as.writeToLog("WARNING: Try to read: " + as.c++ + ". Problem: " + e.toString());
+                        System.out.println("WARNING: Try to read: " + as.c + ". Problem: " + e.toString());
+                        try
+                        {
+                            Thread.sleep(10000);
+                        }
+                        catch (InterruptedException e1)
+                        {
+                            as.writeToLog(e1.toString());
+                        }
+                    }
                     catch (IOException e)
                     {
                         as.writeToLog(e.toString());
                     }
-                }).start();
-            }
-            catch (Exception e)
-            {
-                execute = true;
-                as.writeToLog("WARNING: Try to read: " + c++ + ". " + e.toString());
-                System.out.println("WARNING: Try to read: " + c + ". " + e.toString());
-                Thread.sleep(10000);
-            }
+                }
+            }).start();
         }
-        if(execute) as.writeToLog("ERROR: Failed to read new titles.");
+        catch (Exception e)
+        {
+            as.writeToLog(e.toString());
+        }
+        if(as.execute) as.writeToLog("ERROR: Failed to read new titles.");
 
         //UniversalDetector.main(new String[]{"C:\\Users\\User\\IdeaProjects\\Test\\" + dataBasePath});
     }
 
-    String getNewTitles() throws UnsupportedEncodingException
+    String getNewTitles() throws IOException
     {
         Document doc;
         try
         {
             doc = Jsoup.parse(new URL("http://www.pravda.com.ua/"), 500);
         }
-        catch (IOException e)
+        catch (MalformedURLException e)
         {
-            writeToLog(e.toString());
-            return null;
+            e.printStackTrace();
+            return "";
         }
         doc.charset(Charset.forName("UTF-8"));
         Elements els = doc.getElementsByAttributeValueStarting("class", "news tab_item");
@@ -124,7 +137,6 @@ public class ArticlesStatistics
             writeToLog("ERROR: File " + dataBasePath + " not found.");
             return;
         }
-        if(inFile == null) inFile = "";
         if(newArticles == null) newArticles = "";
         bw.write(newArticles + inFile);
         bw.close();
